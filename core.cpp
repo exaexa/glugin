1b7f7687cc732441b5f2ac8f48e387d5d6dc5e0e
Mirek Kratochvil <exa.exa@gmail.com>
2008-05-07 09:55:31 +0200

    guest_read_host_requests

--- core.cpp
+++ core.cpp
@@ -6,22 +6,51 @@
 
 void guest_read_host_requests(pluginData* pd)
 {
-
+	int cmd;
+	NPStream* s;
+	if(fread(&cmd,sizeof(int),1,pd->pr)<=0) return; //no data
+	switch(cmd) {
+		case gln_new_stream:
+			fread(&s,sizeof(s),1,pd->pr);
+			pd->handle_newstream(pd,s);
+			break;
+		case gln_destroy_stream:
+			fread(&s,sizeof(s),1,pd->pr);
+			pd->handle_destroystream(pd,s);
+			break;
+		case gln_stream_data:
+			{
+				int32 off,len;
+				void*data;
+				fread(&s,sizeof(s),1,pd->pr);
+				fread(&off,sizeof(off),1,pd->pr);
+				fread(&len,sizeof(len),1,pd->pr);
+				if(len>0) {
+					data=malloc(len);
+					fread(data,len,1,pd->pr);
+					pd->handle_write(pd,s,off,len,data);
+					free(data);
+				} else Log("w: zero len!");
+			}
+			break;
+		default: Log("Unhandled request to guest: %d",cmd);
+	}
 }
 
 void guest_newstream(pluginData*pd, NPStream*s)
 {
-
+	Log("guest has NEWSTREAM: %p",s);
 }
 
 void guest_destroystream(pluginData*pd, NPStream*s)
 {
-
+	Log("guest has DESTROYSTREAM: %p",s);
 }
 
 void guest_write(pluginData*pd,NPStream*s,int32 off,int32 len,void*data)
 {
-
+	Log("guest has WRITE: %p %d %d, data followz:",s,off,len);
+	for(int i=0;i<len;++i)Log("%8X: %c",i,((char*)data)[i]);
 }
 
 void glugin_proc(pluginData*pd)
